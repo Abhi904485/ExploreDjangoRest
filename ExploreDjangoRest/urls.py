563bcaf047085917a58c33b00d2d325e8979c54f
akumars1 <abhishek_kumarsingh@mcafee.com>
2020-08-11 14:25:28 +0530

    Updated requirements.txt

--- ExploreDjangoRest/urls.py
+++ ExploreDjangoRest/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
         name='schema-json'),
-    url(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/', include('rest_framework.urls')),
     path('', include('snippets_simple_api.api_urls')),
